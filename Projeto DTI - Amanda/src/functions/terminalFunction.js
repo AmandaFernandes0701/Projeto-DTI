bf8b9403d49b87ec12fdc745c72f2da82284f4b1
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-10-20 00:30:08 -0300

    fix: while loop on terminal

--- Projeto DTI - Amanda/src/functions/terminalFunction.js
+++ Projeto DTI - Amanda/src/functions/terminalFunction.js
@@ -1,22 +1,6 @@
 import readline from 'readline';
 import { calculateBestPetShop } from './calculatePrices.js';
-
 import chalk from 'chalk';
-
-console.log(chalk.magenta.bold('\nProjetinho da Amanda :)  --> DTI'));
-console.log(
-  chalk.bold(
-    '\nSeja muito bem vinda(o)! <3 \nInsira os dados no terminal e descubra qual Pet Shop te fornecerá o melhor custo benefício'
-  )
-);
-console.log(
-  '\n===================================================================================================='
-);
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 function getInput(question) {
   return new Promise((resolve) => {
@@ -25,32 +9,69 @@ function getInput(question) {
 }
 
 async function terminalFunction() {
-  const selectedDate = await getInput(
-    chalk.italic('\nInforme a data no formato dia/mes/ano: ')
+  console.log(chalk.magenta.bold('\n💜 Projetinho da Amanda :)  --> DTI 👩🏾‍💻'));
+  console.log(
+    chalk.bold(
+      '\nSeja muito bem vinda(o)! <3 \nInsira os dados no terminal e descubra qual Pet Shop te fornecerá o melhor custo benefício'
+    )
   );
-  const numSmallDogs = parseInt(
-    await getInput(
-      chalk.italic('\nInsira a quantidade de cachorros pequenos: ')
-    ),
-    10
-  );
-  const numBigDogs = parseInt(
-    await getInput(
-      chalk.italic('\nInsira a quantidade de cachorros grandes: ')
-    ),
-    10
+  console.log(
+    '\n===================================================================================================='
   );
 
-  const bestPetShop = calculateBestPetShop(
-    selectedDate,
-    numSmallDogs,
-    numBigDogs
-  );
-  console.log('\n');
-  console.log(bestPetShop.name);
-  console.log(bestPetShop.price);
+  let repeatAgain = true;
+
+  while (repeatAgain) {
+    const selectedDate = await getInput(
+      chalk.italic('\nInforme a data no formato dia/mes/ano: ')
+    );
+    const numSmallDogs = parseInt(
+      await getInput(
+        chalk.italic('\nInsira a quantidade de cachorros pequenos: ')
+      ),
+      10
+    );
+    const numBigDogs = parseInt(
+      await getInput(
+        chalk.italic('\nInsira a quantidade de cachorros grandes: ')
+      ),
+      10
+    );
+
+    const bestPetShop = calculateBestPetShop(
+      selectedDate,
+      numSmallDogs,
+      numBigDogs
+    );
+
+    console.log('\n');
+    console.log(bestPetShop.name);
+    console.log(bestPetShop.price);
+
+    let resposta = '';
+
+    while (resposta !== 'y' && resposta !== 'n') {
+      resposta = await getInput(
+        chalk.italic('\nDeseja utilizar o programa novamente? (y/n): ')
+      );
+
+      if (resposta.trim().toLowerCase() === 'n') {
+        console.log(
+          '\n🐶 Aproveite o seu dia com seu(s) cachorrinho(s)! 🐾 Bye Bye'
+        );
+        repeatAgain = false;
+      } else if (resposta.trim().toLowerCase() !== 'y') {
+        console.log('\nValor inválido, insira novamente.');
+      }
+    }
+  }
 
   rl.close();
 }
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 terminalFunction();
